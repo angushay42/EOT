53bdce59dd06bfb04b1ca5d84be1587ed7241adb
gubby64 <angushay42@gmail.com>
2024-09-11 13:43:30 +0100

    First Planning Stage complete!
    Had trouble figuring out how the actual code would work.
    First step was thinking how I wanted the input to look, 'Should I use classes and have the inputted
    item i.e. pickaxe class all inherit the optimal order?'
    I chose to instead have classes that will use the string input to check whether all inputted
    enchantments are valid, i.e. no pickaxe can have both Silk Touch AND Fortune, then have a separate function to generate_order
    
    For the past couple days it's been me trying to figure out a more optimal solution than the brute force method of combining each enchantment for every possible combination. I knew such a solution existed because there are choices you can make in-game that can reduce cost, so there isn't a need to try every single combination.
    
    I then found you can make a frequency map of the enchantment order, where the frequency of anvil uses for each book corresponds to the number of books that get used that many times... In an optimised level-tree method of enchanting, which keeps the desired enchanted item at a minimum work penalty (users may want to do this so they can still repair the tool), you can keep the higher costing enchantments at a minimum by only spending levels on them once, and use the cheaper enchantments twice.
    
    Next, I tried to find a method to calculate the cumulative sum of penalties in a level-tree method. This is hard to explain without a diagram, but essentially: since you can only combine two items at a time, that is where you must start. The result of this item will have a penalty of the formula (2^(anvil uses)) - 1. If you combine two items with 0 penalties, this will be 2^0 - 1, or 1 - 1, or 0. However, the resulting item will now have the higher of the two previous penalties, and since both were 0, the result will now have a use count of 1. In a level tree, you can have some imbalances within subtrees, so one side might be calculated normally and the other will have less penalties. I spent way too long looking to find a hidden (potentially non-existent) pattern and ended up just brute forcing the calculation with a double-ended queue.

--- items.py
+++ items.py
@@ -1,10 +1,74 @@
+from collections import deque
 # classes for all tools
 '''
 String input will be something like: Pickaxe ME UB EF5 FO3
 Then, use the pickaxe class to see if the inputted enchants are valid (no mutually exclusive ones like silk touch and fortune) or unassociated enchants like sharpness.
 Next calculate the minimum cost for the input; still working on how to do this, probably some simple DP style implementation as the diagram from the wiki looks like a DAG... 
+
+
+FORMULA: 
+    for each enchantment:
+        (sacrifice - target) * vals[enchantment][1] 
+    for each PWP:
+        2^(prior count) - 1
+
+users separate current enchantments from desired enchantments with "|" 
+
+sort input string into values based on multipliers
+pickaxe FORT3 UNBR3 MEND EFFI5
+
+Should the functions be defined in main.py?
+functions should be in a separate file and then called in main.py, they are utils 
+
+imports are:
+standard,
+third party 
+local, library specific
+
+
+
+
 '''
 class Target():
+    vals = {
+        'PROT': (1,1),
+        'FIPR': (2,1),
+        'FEAT': (2,1),
+        'BLPR': (4,2),
+        'PRPR': (2,1),
+        'THRN': (8,4),
+        'RESP': (4,2),
+        'DPTH': (4,2),
+        'AQUA': (4,2),
+        'SHRP': (1,1),
+        'SMTE': (2,1),
+        'BANE': (2,1),
+        'KNCK': (2,1),
+        'FASP': (4,2),
+        'LOOT': (4,2),
+        'EFFI': (1,1),
+        'SILK': (8,4),
+        'UNBR': (2,1),
+        'FORT': (4,2),
+        'POWR': (1,1),
+        'PNCH': (4,2),
+        'FLME': (4,2),
+        'INFI': (8,4),
+        'LUCK': (4,2),
+        'FRST': (4,2),
+        'MEND': (4,2),
+        'BIND': (8,4),
+        'VNSH': (8,4),
+        'IMPL': (2,1), # JE has x2 the multipliers than bedrock, so use BE as standard.
+        'RIPT': (4,2),
+        'LOYL': (1,1),
+        'CHAN': (8,4),
+        'MLTI': (4,2),
+        'PIER': (1,1),
+        'QUIK': (2,1),
+        'SOUL': (8,4),
+        'SWFT': (8,4),
+        }
     def __init__(self) -> None:
         self.enchants = ['ME', 'UB']
 
@@ -29,6 +93,6 @@ class Pickaxe(Tools):
         super().__init__()
 
 
-x = Pickaxe()
-y = Pickaxe
-print(x.enchants, type(y))
+# x = Pickaxe()
+# y = Pickaxe
+# print(x.enchants, type(y))
---
+++ utils.py
@@ -0,0 +1,41 @@
+from collections import deque
+def freq(size: int) -> dict:
+    '''
+    Returns the frequency of additions for an enchantment tree.
+    '''
+    hmp = {}
+    for i in range(1,size):
+        count = 0
+        n = i
+        while n > 0:
+            n = n&(n-1)
+            count += 1
+        if not count in hmp:
+            hmp[count] = 0
+        hmp[count] += 1
+    return hmp
+
+def pens(n):
+    arr = [0]*n
+    q = deque(arr)
+    ans = 0
+    while len(q) > 1:
+        # pop first 2 elements
+        x,y = q.popleft(), q.popleft()
+
+        pwp = 2**(max(x,y)) - 1
+        if x == y:
+            ans += 2*pwp
+        elif x < y:
+            q.appendleft(max(x,y))
+            q.append(min(x,y))
+            continue
+        else:
+            o_pwp = 2**min(x,y) - 1
+            ans += pwp + o_pwp
+        q.append(max(x,y)+1)
+
+    return ans
+for i in range(1,16):
+    print(i, pens(i))
+# print(7, pens(7))
